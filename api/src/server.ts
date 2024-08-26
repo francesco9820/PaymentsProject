fba0dcac4dec1a56e09158cec5138c43a1938532
francesco9820 <francescodiflumeri1a@gmail.com>
2024-08-27 00:07:58 +0200

    [0001] Use body parser in server

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,10 @@
 import express, { Express, Request, Response } from "express";
 
+import bodyParser from 'body-parser';
+
 const app: Express = express();
+
+app.use(bodyParser.json());
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
