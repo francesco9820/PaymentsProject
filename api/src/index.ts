6f9f713d90278be2fec1451ea33350a3735ea0a8
francesco9820 <francescodiflumeri1a@gmail.com>
2024-08-26 23:51:41 +0200

    [0001] Set up vercel for api deployment

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,14 +4,13 @@ import connectToDatabase from "./connectToDatabase";
 
 import app from "./server";
 
-
 const startServer = async () => {
   dotenv.config();
   await connectToDatabase();
   const port = process.env.PORT || 3000;
 
   app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log(`[server]: Server is running at port ${port}`);
   }); 
 }
 
