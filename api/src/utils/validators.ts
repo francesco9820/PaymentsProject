81e80839410c34b2d34e56fafea242dcd3447b63
francesco9820 <francescodiflumeri1a@gmail.com>
2024-08-29 19:35:58 +0200

    [0002] Create validators

--- api/src/utils/validators.ts
+++ api/src/utils/validators.ts
@@ -2,3 +2,5 @@ export const isString = (value: any): value is string => typeof value === 'strin
 export const isOneOfValues = <T>(constants: Record<string, T>) => (
     value: any,
   ): value is T => Object.values(constants).includes(value);
+export const isNumber = (value: any): value is string => typeof value === 'number';
+export const isBoolean = (value: any): value is string => typeof value === 'boolean';
