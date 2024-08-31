ab7c602eeb0464ef61f9b0d9843f7d60b6f5d51c
francesco9820 <francescodiflumeri1a@gmail.com>
2024-08-31 18:41:27 +0200

    [0003] Set up cors in the backend

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,8 +1,7 @@
 import dotenv from "dotenv";
-
 import express, { Express } from "express";
-
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import handlers from "./handlers";
 
@@ -19,6 +18,14 @@ connectToDatabase();
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+// });
+
+app.use(cors({origin:true,credentials: true}));
 
 app.use("/", handlers);
 
